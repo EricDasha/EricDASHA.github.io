9140c1a84f9d319cee7d9cde1e209a4a59aa147e
EricDasha <146364373+EricDasha@users.noreply.github.com>
2024-10-05 22:24:08 +0800

    咸的

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/20/部分游戏的伪局域网联机/","2024/04/20/一篇没用的文章/"];function toRandomPost(){
+var posts=["2024/09/20/部分游戏的伪局域网联机/","2024/04/20/一篇没用的文章/","2024/10/05/我个人认为意大利面就应该拌42号混凝土/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
